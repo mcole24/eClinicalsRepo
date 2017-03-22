60b3e55cd997cdf5939ff469d2976f260aa2abb0
Matthew Cole <mattaecole@gmail.com>
2017-03-22 09:43:08 -0400

    created connection class for managing SQL connection

---
+++ eClinicals/DAL/DBConnection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace eClinicals.DAL
+{
+    public static class DBConnection
+    {
+
+        public static SqlConnection GetConnection()
+        {
+            try
+            {
+                string connectString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=[CS6232-g5];Integrated Security=True";
+                SqlConnection connect = new SqlConnection(connectString);
+                return connect;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+    }
+}
