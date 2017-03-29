117dca2b4385a392c151e8d3c4576d211101f36e
mstalber1 <mstalbe1@my.westga.edu>
2017-03-28 17:49:14 -0700

    Add :  Logo UPDATE: name of main form to frmMain : because it is already in the views folder UPDATE: form design and background color

---
+++ eClinicals/View/frmMain.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows.Forms;
+
+namespace eClinicals
+{
+    internal class frmLoginPage : Form
+    {
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmLoginPage
+            // 
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImage = global::eClinicals.Properties.Resources.eclinicalLogo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(941, 462);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frmLoginPage";
+            this.Text = "eClinicals Login";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
