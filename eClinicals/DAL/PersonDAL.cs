91ef9ba8f93d796cbd6d5b85308c8a830ce5ae6e
Matthew Cole <mattaecole@gmail.com>
2017-03-30 17:24:01 -0400

    Working on checkPassword method

--- eClinicals/DAL/PersonDAL.cs
+++ eClinicals/DAL/PersonDAL.cs
@@ -44,13 +44,74 @@ namespace eClinicals.DAL
 
 
 
-        public static bool createContactInfo()
+        public static bool createContactInfo(string lName, string fName, DateTime dob, string streetAddress, string city, string state, string zip, string phone, string gender, string ssn)
         {
 
-
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+                    string insertStmt = "INSERT INTO contact (lName, fName, dob, mailingAddressStreet, mailingAddressCity, mailingAddressState, mailingAddressZip, phoneNumber, gender, SSN) " +
+                        "VALUES (@last, @first, @dob, @street, @city, @state, @zip, @phone, @gender, @ssn)";
+                    using (SqlCommand cmd = new SqlCommand(insertStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@last", lName);
+                        cmd.Parameters.AddWithValue("@first", fName);
+                        cmd.Parameters.AddWithValue("@dob", dob);
+                        cmd.Parameters.AddWithValue("@street", streetAddress);
+                        cmd.Parameters.AddWithValue("@city", city);
+                        cmd.Parameters.AddWithValue("@state", state);
+                        cmd.Parameters.AddWithValue("@zip", zip);
+                        cmd.Parameters.AddWithValue("@phone", phone);
+                        cmd.Parameters.AddWithValue("@gender", gender);
+                        cmd.Parameters.AddWithValue("@ssn", ssn);
+                        connect.Open();
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
 
         }
 
+
+        public static bool checkPassword(string username, string password)
+        {
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+
+                    try
+                    {
+                        connect.Open();
+                    }
+                    catch (SqlException e)
+                    {
+                        throw e;
+                    }
+
+                    string selectStmt = "SELECT password FROM logins WHERE userName = @user";
+                    using (SqlCommand cmd = new SqlCommand(selectStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@user", username);
+                        
+                    }
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
 
 
 
