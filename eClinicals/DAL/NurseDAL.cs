ef926d1575e177e1d72f37dc5069984c298f2bce
Matthew Cole <mattaecole@gmail.com>
2017-03-29 22:40:55 -0400

    Added createNurseLogin method

--- eClinicals/DAL/NurseDAL.cs
+++ eClinicals/DAL/NurseDAL.cs
@@ -13,18 +13,10 @@ namespace eClinicals.DAL
 {
     class NurseDAL
     {
-        /**
-            public static NurseDAL NurseLogin(string userName, string password)
-            {
-
-                SimpleAES encrypt = new SimpleAES();
+        private SimpleAES encrypt = new SimpleAES();
 
 
-            }
-        **/
-
-
-        public static bool createNurse(int contactID, Nurse newNurse)
+        public static bool createNurse(int contactID)
         {
 
             try
@@ -35,6 +27,33 @@ namespace eClinicals.DAL
                     using (SqlCommand cmd = new SqlCommand(insertStmt, connect))
                     {
                         cmd.Parameters.AddWithValue("@contact", contactID);
+                        connect.Open();
+                        cmd.ExecuteNonQuery();
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+
+        public static bool createNurseLogin(int contactID, string username, string password)
+        {
+
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+                    string insertStmt = "INSERT INTO logins (contactID, userName, password) VALUES (@contact, @user, @password);";
+                    using (SqlCommand cmd = new SqlCommand(insertStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@contact", contactID);
+                        cmd.Parameters.AddWithValue("@user", username);
+                        cmd.Parameters.AddWithValue("@password", password);
                         connect.Open();
                         cmd.ExecuteNonQuery();
                         return true;
