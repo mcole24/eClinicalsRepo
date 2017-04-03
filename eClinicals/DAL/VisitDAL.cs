c4fec1a4c777bc786f839204b95a22aa6af4c1e9
Bianca Robinson <brobin16@my.westga.edu>
2017-04-03 19:26:18 -0400

    added GetTestResults method to VisitDAL to return list of patient test results, added properties in the LabTest view to match.

--- eClinicals/DAL/VisitDAL.cs
+++ eClinicals/DAL/VisitDAL.cs
@@ -53,5 +53,60 @@ namespace eClinicals.DAL
             }
             return checkResultList;
         }
+
+        public static List<LabTest> GetTestResults(int patientID)
+        {
+            List<LabTest> testResultsList = new List<LabTest>();
+            string selectStatement = "SELECT testID, testDateCompleted, testType, initialDiagnosis, finalDiagnosis, diagnosisType, lName "
+            + "FROM appointment JOIN visit ON appointment.appointmentID = visit.appointmentID "
+            + "JOIN visit_lab_test ON visit.visitID = visit_lab_test.visitID "
+            + "JOIN lab_test ON visit_lab_test.testCode = lab_test.testCode "
+            + "JOIN visit_has_diagnosis ON visit_lab_test.visitID = visit_has_diagnosis.visitID "
+            + "JOIN diagnosis ON visit_has_diagnosis.diagnosisID = diagnosis.diagnosisID "
+            + "JOIN doctor ON appointment.doctorID = doctor.doctorID "
+            + "JOIN contact ON doctor.contactID = contact.contactID "
+            + "WHERE patientID = @patientID";
+            try
+            {
+                using (SqlConnection connection = DBConnection.GetConnection())
+                {
+                    connection.Open();
+                    using (SqlCommand selectCommand = new SqlCommand(selectStatement, connection))
+                    {
+                        selectCommand.Parameters.AddWithValue("@patientID", patientID);
+                        using (SqlDataReader reader = selectCommand.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                LabTest testResult = new LabTest();
+                                testResult.TestID = (int)reader["testID"];
+                                testResult.PerformedDate = (DateTime)reader["testDateCompleted"];
+                                testResult.Test = reader["testType"].ToString();
+                                testResult.InitialDiagnosis = (int)reader["initialDiagnosis"];
+                                testResult.FinalDiagnosis = (int)reader["finalDiagnosis"];
+                                testResult.DiagnosisType = reader["diagnosisType"].ToString();
+                                testResult.Doctor = reader["lName"].ToString();
+                           
+                                testResultsList.Add(testResult);
+                            }
+                            reader.Close();
+                        }
+
+                    }
+                    connection.Close();
+                }
+            }
+
+            catch (SqlException ex)
+            {
+                throw;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return testResultsList;
+        }
+
     }
 }
