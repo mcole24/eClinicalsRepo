734ba29cc102033374cd0c20326cb425076f99e3
Bianca Robinson <brobin16@my.westga.edu>
2017-04-03 01:32:38 -0400

    Added GetPreviousReadings to VisitDAL. Added RoutineCheck model class for routine check data objects.

---
+++ eClinicals/Model/RoutineCheck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eClinicals.Model
+{
+    class RoutineCheck
+    {
+        public DateTime VisitDate { get; set; }
+
+        public int DiastolicBP { get; set; }
+
+        public int SystolicBP { get; set; }
+
+        public decimal BodyTemperature { get; set; }
+
+        public int Pulse { get; set; }
+
+        public string Symptom { get; set; }
+    }
+}
