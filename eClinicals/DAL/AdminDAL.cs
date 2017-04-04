8f880df47769ed8726cac8baaec0ab4cdf88add6
Bianca Robinson <brobin16@my.westga.edu>
2017-04-04 00:31:17 -0400

    added AdminDAL with method GetAdminByID and added properties of Admin to Model class for Admin.

---
+++ eClinicals/DAL/AdminDAL.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using eClinicals.Model;
+using System.Data.SqlClient;
+
+namespace eClinicals.DAL
+{
+    class AdminDAL
+    {
+        public static Admin GetAdminByID(int contactID)
+        {
+            Admin admin = new Admin();
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+                    connect.Open();
+                    string selStmt = "SELECT * FROM contact INNER JOIN admin ON contact.contactID = admin.contactID WHERE admin.contactID = @contactID";
+                    using (SqlCommand cmd = new SqlCommand(selStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@contactID", contactID);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                admin.AdminID = (int)reader["adminID"];
+                                admin.ContactID = (int)reader["contactID"];
+                                admin.LastName = reader["lName"].ToString();
+                                admin.FirstName = reader["fName"].ToString();
+                                admin.Dob = (DateTime)reader["dob"];
+                                admin.Address = reader["mailingAddressStreet"].ToString();
+                                admin.City = reader["mailingAddressCity"].ToString();
+                                admin.State = reader["mailingAddressState"].ToString();
+                                admin.Zip = reader["mailingAddressZip"].ToString();
+                                admin.Phone = reader["phone"].ToString();
+                                admin.Gender = reader["gender"].ToString();
+                                admin.Ssn = reader["ssn"].ToString();
+                                admin.UserName = reader["username"].ToString();
+                            }
+                            reader.Close();
+                        }
+                    }
+                    connect.Close();
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return admin;
+        }
+    }
+}
--- eClinicals/Model/Admin.cs
+++ eClinicals/Model/Admin.cs
@@ -8,5 +8,18 @@ namespace eClinicals.Model
 {
     class Admin
     {
+        public string Address { get;  set; }
+        public int AdminID { get;  set; }
+        public string City { get;  set; }
+        public string Zip { get; set; }
+        public int ContactID { get;  set; }
+        public DateTime Dob { get;  set; }
+        public string FirstName { get;  set; }
+        public string Gender { get;  set; }
+        public string LastName { get;  set; }
+        public string Phone { get;  set; }
+        public string Ssn { get;  set; }
+        public string State { get;  set; }
+        public string UserName { get;  set; }
     }
 }
