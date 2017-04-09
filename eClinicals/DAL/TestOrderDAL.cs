0b41a56bc25710bbe5daf420afa1540af7edb48b
Matthew Cole <mattaecole@gmail.com>
2017-04-09 13:51:57 -0400

    Created OrderTest and GetTestByID methods in TestOrderDAL

---
+++ eClinicals/DAL/TestOrderDAL.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using eClinicals.DAL;
+using eClinicals.Model;
+
+namespace eClinicals.DAL
+{
+    class TestOrderDAL
+    {
+
+        public static int OrderTest(TestOrder testOrdered)
+        {
+            string insertStmt = "INSERT INTO visit_lab_test (testCode, visitID, result, testDateCompleted) " + 
+                "VALUES (@code, @vID, @result, @dateCompleted)";
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+                    connect.Open();
+                    using (SqlCommand cmd = new SqlCommand(insertStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@code", testOrdered.TestCode);
+                        cmd.Parameters.AddWithValue("@vID", testOrdered.VisitID);
+                        cmd.Parameters.AddWithValue("@result", testOrdered.Result);
+                        cmd.Parameters.AddWithValue("@dateCompleted", testOrdered.DateCompleted);
+                        cmd.ExecuteNonQuery();
+                        SqlCommand idCmd = new SqlCommand();
+                        idCmd.Connection = connect;
+                        idCmd.CommandText = "SELECT IDENT_CURRENT('testOrdered') FROM visit_lab_test";
+                        int testID = Convert.ToInt32(idCmd.ExecuteScalar());
+                        connect.Close();
+                        return testID;
+                    }
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+
+
+        public static TestOrder GetTestByID(int testID)
+        {
+            TestOrder orderedTest = new Model.TestOrder();
+            string selectStmt = "SELECT testID, testCode, visitID, result, testDateCompleted FROM visit_lab_test WHERE testID = @testID";
+            try
+            {
+                using (SqlConnection connect = DBConnection.GetConnection())
+                {
+                    connect.Open();
+                    using (SqlCommand cmd = new SqlCommand(selectStmt, connect))
+                    {
+                        cmd.Parameters.AddWithValue("@testID", testID);
+                        using (SqlDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                orderedTest.TestID = (int)reader["testID"];
+                                orderedTest.TestCode = reader["testCode"].ToString();
+                                orderedTest.VisitID = (int)reader["visitID"];
+                                orderedTest.Result = (Boolean)reader["result"];
+                                orderedTest.DateCompleted = (DateTime)reader["testDateCompleted"];
+
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException sqlex)
+            {
+                throw sqlex;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return orderedTest;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ eClinicals/Model/TestOrder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eClinicals.Model
+{
+    class TestOrder
+    {
+
+        public int TestID { get; set; }
+        public string TestCode { get; set; }
+        public int VisitID { get; set; }
+        public Boolean Result { get; set; }
+        public DateTime DateCompleted { get; set; }
+
+    }
+}
