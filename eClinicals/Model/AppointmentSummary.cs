075d9da1abf1f1d4d3b51d6e7740969f6ac29bd8
Bianca Robinson <brobin16@my.westga.edu>
2017-04-18 04:22:46 -0400

    updated object for appointment summary methods in AppointmentDAL to AppointmentSummary

--- eClinicals/Model/AppointmentSummary.cs
+++ eClinicals/Model/AppointmentSummary.cs
@@ -16,5 +16,25 @@ namespace eClinicals.Model
 
     public string ReasonForVisit { get; set; }
 
+    public string Symptoms { get; set; }
+
+   
+        public int SystolicBPReading { get; set; }
+        public int DiastolicBPReading { get; set; }
+
+        public decimal BodyTemperatureReading { get; set; }
+
+        public int PulseReading { get; set; }
+
+        public string InitialDiagnosis { get; set; }
+
+        public string FinalDiagnosis { get; set; }
+
+        public string TestResult { get; set; }
+
+        public string TestName { get; set; }
+
+        public bool ResultRecorded { get; set; }
+
     }
 }
