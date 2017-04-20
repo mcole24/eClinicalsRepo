a18507af5e215210af8fa613f11257ebdbe3f1af
Matthew Cole <mattaecole@gmail.com>
2017-04-20 04:56:42 -0400

    Modified CreateCheckup to allow for connection to close.

--- eClinicals/DAL/VisitDAL.cs
+++ eClinicals/DAL/VisitDAL.cs
@@ -77,9 +77,10 @@ namespace eClinicals.DAL
                         cmd.Parameters.AddWithValue("@dBP", diastolicBP);
                         cmd.Parameters.AddWithValue("@temp", bodyTemp);
                         cmd.Parameters.AddWithValue("@pulse", pulse);
+                        connect.Close();
                         return (cmd.ExecuteNonQuery() > 0);
                     }
-                    connect.Close();
+                    
                 }
             }
             catch
