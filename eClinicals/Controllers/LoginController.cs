01a0909b0cee1dae6a7fe579df33fad3d406c25c
mstalber1 <mstalbe1@my.westga.edu>
2017-04-28 22:26:20 -0700

    Replace password

--- eClinicals/Controllers/LoginController.cs
+++ eClinicals/Controllers/LoginController.cs
@@ -30,12 +30,13 @@ namespace eClinicals.Controllers
         {
             // nurse : cwoods6
             //admin : jwynn1
-            //TODO: replace this with commented area below
-            frmLoginView.username = "cwoods6";
-            frmLoginView.password = "testpassword123";
-            // frmLoginView.username = frmLoginView.txtUserName.Text;
-            //   frmLoginView.password = frmLoginView.txtPassword.Text;
-            //   frmLoginView.username = frmLoginView.txtUserName.Text;
+            //TODO: PASSWORD - replace this with commented area below
+
+            // frmLoginView.username = "cwoods6";
+            // frmLoginView.password = "testpassword123";
+            frmLoginView.username = frmLoginView.txtUserName.Text;
+            frmLoginView.password = frmLoginView.txtPassword.Text;
+
             LogIn(frmLoginView.username, frmLoginView.password);
         }
         //define delegate
